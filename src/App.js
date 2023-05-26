a58a18e979e1cf5275924a8cb2d46845e061a8c2
SEERRGEE <ieg.sergo@gmail.com>
2023-05-26 13:20:28 -0400

    prohibit unauthorised users from adding new gym

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import Gyms from './components/Gyms/Gyms';
 import GymDetails from './components/GymDetails/GymDetails';
 import AddGym from './components/AddGym/AddGym';
 import Login from './components/Login/Login';
+import { useCookies } from 'react-cookie';
 
 function App() {
+
+  const [cookies, setCooikes] = useCookies(['access_token'])
+
+  const logout = () => {
+    setCooikes("access_token", "");
+    window.localStorage.removeItem('userID');
+    // navigate back to login
+  }
+
+  
+
   return (
     <BrowserRouter>
       <main>
         <Routes>
+          { cookies.access_token ? <Route path='/add' element={<AddGym/>}/> : <Route path='/add' element={<Zip/>}/> }
           <Route path='/' element={<Zip/>}/>
           <Route path='/enter' element={<Enter/>}/>
           <Route path='/gyms' element={<Gyms/>}/>
           <Route path='/gyms/:id' element={<GymDetails/>}/>
-          <Route path='/add' element={<AddGym/>}/>
           <Route path='/login' element={<Login/>}/>
         </Routes>
       </main>
